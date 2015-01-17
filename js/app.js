1290d090aa6b50da9d7d808d4544e62a3d0ef6ed
Mauro Bianchi <bianchimro@gmail.com>
2015-01-17 17:04:43 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+angular.module('mapz', [])
+
+.controller('TestCtrl', ['$scope', function ($scope) {
+    var u = new Gh3.User("inmagik");
+    var repo = new Gh3.Repository("k33g.github.com", k33g);
+    
+}])
+
+.configure()
+.run();
